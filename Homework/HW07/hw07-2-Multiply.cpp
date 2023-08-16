b22027565038ec0b0154bf6fa19f49701390b4d8
zarlipzz <guitar30255@gmail.com>
2023-08-17 02:23:19 +0700

    workkk

--- Homework/HW07/hw07-2-Multiply.cpp
+++ Homework/HW07/hw07-2-Multiply.cpp
@@ -75,3 +75,24 @@
         6 x 9 = 54
 
 */
+#include <stdio.h>
+
+int main(){
+    
+    int str,end,i,i2;
+    printf("Start :\n");
+    scanf("%d",&str );
+    printf("End :\n");
+    scanf("%d",&end);
+    i = str;
+    while (i <= end){
+        i2 = 1;
+        while (i2 <= 9){
+            printf("%d x %d = %d\n",i,i2,i*i2);
+            i2++;
+        }
+        i++;
+        printf("\n");
+    }
+    return 0;
+}
